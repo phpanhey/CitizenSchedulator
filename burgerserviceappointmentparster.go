0d3a8284b112d9465f1c8d1d5010181fa0893e5b
ppp <philipp.panhey@gmail.com>
2024-11-15 08:01:49 +0100

    wip stable

--- burgerserviceappointmentparster.go
+++ burgerserviceappointmentparster.go
@@ -16,24 +16,26 @@ func main() {
 	url := "https://www.service.bremen.de/dienstleistungen/reisepass-beantragen-fuer-personen-unter-18-jahren-126498"
 	urlContent := getUrlContent(url)
 	// a very high nextAvailableAppointmentDate to make sure the parser gets the next nextAvailableAppointmentDate
-	nextAvailableAppointmentDate := ParseTimeStamp(CleanDate("Do. 05.12.30 um 10:00"))
+	nextAvailableAppointmentDate := ParseTimeStamp("05.12.30 10:00")
 
 	parts := strings.Split(urlContent, "Frühestmöglicher Termin in Bremen:")
 	if len(parts) > 1 {
 		doc := soup.HTMLParse(strings.Split(urlContent, "Frühestmöglicher Termin in Bremen:")[1])
 		links := doc.FindAll("a")
+
 		nextAvailableAppointmentDate = ParseTimeStamp(CleanDate(links[0].Text()))
 	}
 
 	if nextAvailableAppointmentDate.Before(TimeFromFile("date.txt")) {
 		fmt.Println("Next available appointment: ", nextAvailableAppointmentDate)
-
+		os.WriteFile("date.txt", []byte(nextAvailableAppointmentDate.Format("02.01.06 15:04")), 0644)
 	}
 
 }
 
 func ParseTimeStamp(date string) time.Time {
-	timeStamp, err := time.Parse("02.01.06 um 15:04", date)
+	layout := "02.01.06 15:04"
+	timeStamp, err := time.Parse(layout, date)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,12 +45,14 @@ func ParseTimeStamp(date string) time.Time {
 func TimeFromFile(s string) time.Time {
 	content, err := os.ReadFile(s)
 	if err != nil {
-		log.Fatal(err)
+		os.WriteFile("date.txt", []byte("05.12.30 10:00"), 0644)
 	}
 	return ParseTimeStamp(string(content))
 }
 
 func CleanDate(date string) string {
+	// also get rid of "um" in string
+	date = strings.Replace(date, "um", "", -1)
 	return strings.TrimSpace(date[4:])
 }
 
